caa3252dd15031f24df9d07e0b145a4e923594b4
MichaelSanchez2007 <michaelsanchez7898@gmail.com>
2021-08-20 17:10:54 -0400

    first commit

---
+++ src/routes/index.svelte
@@ -0,0 +1,62 @@
+<script>
+    function superclose() {
+        window.close();
+    }
+    function black() {
+        alert("im in your walls");
+    }
+    
+</script>
+
+<button on:click={black}>I CORER IT</button>
+<button on:click={superclose}>DGHI</button>
+
+<h1>ITS YOUR PARENTS</h1>
+<h2>SWAG</h2>
+
+<style>
+    :global(body) {
+        transition: 5s;
+        background: brown;
+    }
+h1 {
+    text: green;
+    background-color: blue;
+    transition: 20s;
+    height: 100px;
+    background: blueviolet;
+    background: red;
+}
+h1:hover {
+    transform: scale(50);
+}
+img {
+    z-index: -1;
+    background: black;
+    margin: auto;
+    display: flex;
+    z-index: 11;
+}
+h2 {
+    color: red;
+    background: black;
+    width: 100px;
+    text-align: center;
+    margin: auto;
+    border-radius: 25px;
+    transition: 50s;
+}
+h2:hover {
+    width: 100rem;
+    height: 100rem;
+    transform: scale(100px);
+}
+button {
+    cursor: pointer;
+    background: rgb(243, 156, 43);
+}
+</style>
+
+<img src="/FREDDY.png"> 
+<img src="/Spring.png"> 
+<img src="/Alien.png"> 
