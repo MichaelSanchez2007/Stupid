961d39c011ff69d872e3cf665aa0aea8c4289d68
MichaelSanchez2007 <michaelsanchez7898@gmail.com>
2021-08-23 21:11:19 -0400

    FIXED CHANGED AMAZING

---
+++ src/beam/BeamA.svelte
@@ -0,0 +1,10 @@
+<svg width="49" height="254" viewBox="0 0 49 254" fill="none" xmlns="http://www.w3.org/2000/svg">
+    <rect x="46.2583" width="253.524" height="46.26" rx="23.13" transform="rotate(89.5 46.2583 0)" fill="url(#paint0_linear)"/>
+    <defs>
+    <linearGradient id="paint0_linear" x1="38.9782" y1="21.2661" x2="343.354" y2="18.9728" gradientUnits="userSpaceOnUse">
+    <stop stop-color="#0038FF"/>
+    <stop offset="0.854167" stop-color="#2958FF" stop-opacity="0"/>
+    </linearGradient>
+    </defs>
+    </svg>
+    
--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,62 +1,72 @@
 <script>
-    function superclose() {
+    import BeamA from "../beam/BeamA.svelte";
+
+    function windowcloser() {
         window.close();
     }
-    function black() {
-        alert("im in your walls");
-    }
-    
 </script>
 
-<button on:click={black}>I CORER IT</button>
-<button on:click={superclose}>DGHI</button>
+<div class="home-page">
 
-<h1>ITS YOUR PARENTS</h1>
-<h2>SWAG</h2>
+    <h1>FREDDY VS AMONG US</h1>
+
+        <div class="container">
+
+            <h2>SELECTOR</h2>
+            <button on:click={showBeam}>BEAMER</button>
+            <button on:click={windowcloser}>DONT DO IT</button>
+            
+            <div id="beam-a"><BeamA /></div>
+            
+            
+            <div class="fighters">
+                
+                <img src="/Fred.png" alt="">
+                <img src="/Among.png" alt="">
+                
+        </div>
+    </div>
+</div>
 
 <style>
     :global(body) {
-        transition: 5s;
-        background: brown;
+    background: linear-gradient(180deg, #000000 0%, rgba(0, 0, 0, 0) 100%);
+    background-repeat: no-repeat;
     }
-h1 {
-    text: green;
-    background-color: blue;
-    transition: 20s;
-    height: 100px;
-    background: blueviolet;
-    background: red;
-}
-h1:hover {
-    transform: scale(50);
-}
-img {
-    z-index: -1;
-    background: black;
-    margin: auto;
-    display: flex;
-    z-index: 11;
-}
-h2 {
-    color: red;
-    background: black;
-    width: 100px;
-    text-align: center;
-    margin: auto;
-    border-radius: 25px;
-    transition: 50s;
-}
-h2:hover {
-    width: 100rem;
-    height: 100rem;
-    transform: scale(100px);
-}
-button {
-    cursor: pointer;
-    background: rgb(243, 156, 43);
-}
-</style>
 
-<img src="/FREDDY.png"> 
-<img src="/Spring.png"> 
-<img src="/Alien.png"> 
+    h1 {
+        color: rgb(0, 0, 0);
+        background: rgb(255, 0, 0);
+        width: 400px;
+    }
+    img {
+        width: 100px;
+        margin: auto;
+        display: flex;
+        justify-content: space-between;
+        padding: 5.5rem 5.25rem;
+    }
+    h2 {
+        text-align: center;
+        color: red;
+    }
+    .container {
+        background: linear-gradient(180deg, #5D948A 0%, rgba(247, 2, 2, 0) 100%);
+        border-radius: 20px;
+        width: 600px;
+        margin: auto;
+    }
+    button {
+        margin: auto;
+        display: flex;
+        padding: 5px;
+        color: royalblue;
+        background: black;
+        cursor: pointer;
+    }
+    #beam-a {
+        position: absolute;
+        right: 28rem;
+        bottom: 370px;
+    }
+    </style>
